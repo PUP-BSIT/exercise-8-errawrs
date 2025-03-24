df5e1c970e23392412e7f88687449a9e7479ee5b
delumen-ivan <ivandelumen05@gmail.com>
2025-03-24 14:01:34 +0800

    add comment functionality in js

--- delumen_ivan/script.js
+++ delumen_ivan/script.js
@@ -1,6 +1,11 @@
 let input = document.querySelector(".name");
 let textarea = document.querySelector("textarea");
 let button = document.querySelector(".submit-button");
+let commentSection = document.querySelector(".teammate-comment");
+
+button.addEventListener('click', addComment);
+input.addEventListener('input', checkValue);
+textarea.addEventListener('input', checkValue);
 
 function checkValue() {
   if (input.value && textarea.value) {
@@ -14,5 +19,17 @@ function checkValue() {
   }
 }
 
-input.addEventListener("input", checkValue);
-textarea.addEventListener("input", checkValue);
+function addComment() {
+  let commentContainer = document.createElement('div');
+  commentContainer.classList.add("comment-item");
+
+  let commentName = document.createElement('strong');
+  commentName.innerHTML = input.value;
+
+  let commentText = document.createElement('p');
+  commentText.innerHTML = textarea.value
+
+  commentSection.append(commentContainer);
+  commentContainer.append(commentText);
+  commentContainer.append(commentName);
+}
