e9d595251391546e74e0dd6d211cfefe400e6911
delumen-ivan <ivandelumen05@gmail.com>
2025-03-24 08:32:50 +0800

    add semicolon to js file

--- delumen_ivan/script.js
+++ delumen_ivan/script.js
@@ -10,7 +10,7 @@ function checkValue() {
   } else {
     button.disabled = true;
     button.classList.remove("enable");
-    button.classList.add("disabled")
+    button.classList.add("disabled");
   }
 }
 
